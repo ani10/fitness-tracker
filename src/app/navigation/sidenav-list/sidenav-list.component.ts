82c1300e4e0fb6a9acb7c241570595386f0d2848
Anirban Sinha <get.ani@outlook.com>
2019-09-03 18:09:27 +0530

    authentication service created and implemented(mock)

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  authSubscription: Subscription;
+  isAuth = false;
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
   onClose() {
     this.closeSidenav.emit();
   }
 
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
 }
